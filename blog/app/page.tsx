140180f8e100350775b2a0184bb917436a3f06de
nicolas leprince <nicolas.leprince20@orange.fr>
2024-11-25 14:14:51 +0100

    ajout : presentation des articles du plus recent au plus ancien

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,7 +1,25 @@
+import { prisma } from "../src/db/prisma";
+
 export default async function Home() {
-  return(
+  // Récupérer tous les utilisateurs
+  const allArticles = await prisma.article.findMany({orderBy: {date: 'desc'}});
+
+  return (
     <div>
-      <p>page d'accueil</p>
-     </div>
-  )
+      <h1>Les Articles les plus récent</h1>
+      <ul>
+        {allArticles.map((article) => (
+          <li key={article.id}>
+            <div>
+              <p>titre : {article.titre}</p>
+            </div>
+            <div>
+              <p>auteur : {article.utilisateur.pseudo}</p>
+              <p>date publication : {article.date.toDateString()}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
